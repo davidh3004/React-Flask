4061fcf63090d14f89c0b291c10d5fe35650dcc2
davidh3004 <henriquezdavid3004@gmail.com>
2025-03-28 18:49:27 -0400

    Modifications to React file, receiving data from Backend(Python)

--- server-flask/main.py
+++ server-flask/main.py
@@ -11,7 +11,16 @@ def hello_world():
 @app.route('/api/users')
 def get_users():
     return {
-        "users": ['Alice', 'Bob', 'Charlie'],
+        "users": [
+            {'id': 1,
+             'name': 'Alice'
+            },
+            {'id': 2,
+             'name': 'Bob'
+            },
+            {'id': 3,
+             'name': 'Juan'
+            }],
     }
 
 @app.route('/api/fruits')
